e06c8b24af7ec1b67ebad2d129bf5e40a962dc9b
Rafael Candeias <rafaelcandeias39@gmail.com>
2022-12-14 14:50:57 -0300

    @RafaelCandeias00 [Task: add models]

---
+++ CatalogoAPI-Net5/CatalogoAPI-Net5/Models/Categoria.cs
@@ -0,0 +1,9 @@
+﻿namespace CatalogoAPI_Net5.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string Nome { get; set; }
+        public string ImagemUrl { get; set; }
+    }
+}
