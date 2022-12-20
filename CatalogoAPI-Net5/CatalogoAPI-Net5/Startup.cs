6583dd65044a5a26b8311913ef0015623f6ae969
RafaelCandeias00 <rafaelcandeias39@gmail.com>
2022-12-20 18:47:24 -0300

    @RafaelCandeias00 [Task: habilitando CORS]

--- CatalogoAPI-Net5/CatalogoAPI-Net5/Startup.cs
+++ CatalogoAPI-Net5/CatalogoAPI-Net5/Startup.cs
@@ -5,21 +5,14 @@ using CatalogoAPI_Net5.Repository;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace CatalogoAPI_Net5
 {
@@ -35,6 +28,8 @@ namespace CatalogoAPI_Net5
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
+
             var mappingConfig = new MapperConfiguration(mc =>
             {
                 mc.AddProfile(new MappingProfile());
@@ -117,15 +112,29 @@ namespace CatalogoAPI_Net5
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CatalogoAPI_Net5 v1"));
             }
 
+            // Middleware para redirecionar para https
             app.UseHttpsRedirection();
 
+            // Adiciona o middleware de roteamento
             app.UseRouting();
 
+            // Adiciona o middleware de autenticação
             app.UseAuthentication();
+
+            // Adiciona o middleware de autorização
             app.UseAuthorization();
 
+            // Habilitando o CORS
+            app.UseCors(c => c
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+            );
+
+            // Adiciona o middleware que executa o endpoint do request atual
             app.UseEndpoints(endpoints =>
             {
+                // Adiciona os endpoints para as Actions dos controladores sem especificar rotas
                 endpoints.MapControllers();
             });
         }
