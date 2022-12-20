80215641f2e07ed8cd23a0f3a85fb00a0d4f12d0
RafaelCandeias00 <rafaelcandeias39@gmail.com>
2022-12-20 18:50:03 -0300

    @RafaelCandeias00 [Task: descomentando autorização de CategoriasController]

--- CatalogoAPI-Net5/CatalogoAPI-Net5/Controllers/CategoriasController.cs
+++ CatalogoAPI-Net5/CatalogoAPI-Net5/Controllers/CategoriasController.cs
@@ -19,7 +19,7 @@ namespace CatalogoAPI_Net5.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class CategoriasController : ControllerBase
     {
         private readonly IUnitOfWork _uof;
